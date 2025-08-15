8747bc73eda2498cc7cd7914f04a2cf32d536d6c
Ian Lansdowne <idlansdowne@gmail.com>
2025-08-15 01:55:59 -0500

    Fixed animation issue

--- _tools/generate_stars.py
+++ _tools/generate_stars.py
@@ -1,31 +1,42 @@
 from numpy.random import normal, uniform
 from numpy import clip, maximum
 
+w = 1920
+h = 1080
+
 n = 500
 size = maximum(normal(1.5, 0.1, n), 0)
-x = uniform(0, 1920, n)
-y = uniform(0, 1080, n)
+x = uniform(0, w, n)
+y = uniform(0, h, n)
 value = clip(normal(255, 5, n), 0, 255).astype(int)
 stars = zip(x, y, size, value)
 
-n = 32
+stars_svg = f"""
+<svg xmlns="http://www.w3.org/2000/svg" width="{w}" height="{h}" viewBox="0 0 {w} {h}">
+  {"\n".join([f'<circle r="{size}" cx="{x}" cy="{y}" fill="#{v:02x}{v:02x}{v:02x}"/>' for x, y, size, v in stars])}
+</svg>
+"""
+
+with open("stars.svg", "w") as file:
+  file.write(stars_svg)
+
+n = 20
+blur = 15
 size = maximum(normal(40, 10, n), 0)
-x = uniform(-1920, 1920, n)
-y = uniform(0, 1080, n)
+x = uniform(0, w, n)
+y = uniform(0, h, n)
 twinkle = zip(x, y, size)
 
-
-svg = f"""
-<svg xmlns="http://www.w3.org/2000/svg" width="1920" height="1080" viewBox="0 0 1920 1080">
+twinkle_svg = f"""
+<svg xmlns="http://www.w3.org/2000/svg" width="{w}" height="{h}" viewBox="0 0 {w} {h}">
   <filter id="blur" x="-50%" y="-50%" width="200%" height="200%">
-    <feGaussianBlur in="SourceGraphic" stdDeviation="15" />
+    <feGaussianBlur in="SourceGraphic" stdDeviation="{blur}" />
   </filter>
-  {"\n".join([f'<circle r="{size}" cx="{x}" cy="{y}" fill="#{v:02x}{v:02x}{v:02x}"/>' for x, y, size, v in stars])}
-  <g id="twinkle" fill="#00000" filter="url(#blur)">
-    <animateMotion dur="120s" repeatCount="indefinite" path="M0,0 L1920,0 z" />
-    {"\n".join([f'<circle r="{size}" cx="{x}" cy="{y}"/>' for x, y, size in twinkle])}
+  <g fill="#00000" filter="url(#blur)">
+    {"\n".join([f'<circle r="{r}" cx="{x}" cy="{y}"/>' for x, y, r in twinkle if 0 < x-r-blur and x+r+blur < w and 0 < y-r-blur and y+r+blur < h])}
   </g>
 </svg>
 """
-with open("stars.svg", "w") as file:
-    file.write(svg)
+
+with open("twinkling.svg", "w") as file:
+  file.write(twinkle_svg)
